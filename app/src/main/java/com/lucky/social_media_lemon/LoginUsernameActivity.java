9ce1dff491c2bb740b397f96bcde667b8eeebe55
ducloc <locdivine@gmail.com>
2023-06-10 15:34:01 +0700

    Initial commit

---
+++ app/src/main/java/com/lucky/social_media_lemon/LoginUsernameActivity.java
@@ -0,0 +1,95 @@
+package com.lucky.social_media_lemon;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.lucky.social_media_lemon.model.UserModel;
+import com.lucky.social_media_lemon.utils.FirebaseUtil;
+
+public class LoginUsernameActivity extends AppCompatActivity {
+
+    EditText usernameInput;
+    Button letMeInBtn;
+    ProgressBar progressBar;
+    String phoneNumber;
+    UserModel userModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_username);
+
+        usernameInput = findViewById(R.id.login_username);
+        letMeInBtn = findViewById(R.id.login_let_me_in_btn);
+        progressBar = findViewById(R.id.login_progress_bar);
+
+        phoneNumber = getIntent().getExtras().getString("phone");
+        getUsername();
+
+        letMeInBtn.setOnClickListener(v->{
+            setUsername();
+        });
+    }
+    void setUsername(){
+        setInProgress(true);
+        String username = usernameInput.getText().toString();
+        if (username.isEmpty() || username.length()<3){
+            usernameInput.setError("Username length should be at least 3 chars");
+            return;
+        }
+        if (userModel!=null){
+            userModel.setUsername(username);
+        } else {
+            userModel = new UserModel(phoneNumber, username, Timestamp.now(), FirebaseUtil.currentUserId());
+        }
+
+        FirebaseUtil.currentUserDetails().set(userModel).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                setInProgress(false);
+                if (task.isSuccessful()){
+                    Intent intent = new Intent(LoginUsernameActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+    void getUsername(){
+        setInProgress(true);
+        FirebaseUtil.currentUserDetails().get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                setInProgress(false);
+                if (task.isSuccessful()){
+                    UserModel userModel = task.getResult().toObject(UserModel.class);
+                    if (userModel!=null){
+                        usernameInput.setText(userModel.getUsername());
+                    }
+                }
+            }
+        });
+    }
+
+    void setInProgress(boolean inProgress){
+        if (inProgress){
+            progressBar.setVisibility(View.VISIBLE);
+            letMeInBtn.setVisibility(View.INVISIBLE);
+        } else {
+            progressBar.setVisibility(View.INVISIBLE);
+            letMeInBtn.setVisibility(View.VISIBLE);
+        }
+    }
+}
---
+++ app/src/main/java/com/lucky/social_media_lemon/utils/FirebaseUtil.java
@@ -0,0 +1,46 @@
+package com.lucky.social_media_lemon.utils;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class FirebaseUtil {
+
+    public static String currentUserId(){
+
+        return FirebaseAuth.getInstance().getUid();
+    }
+
+    public static boolean isLoggedIn(){
+        if (currentUserId()!=null){
+            return true;
+        }
+        return false;
+    }
+
+    public static DocumentReference currentUserDetails(){
+        return FirebaseFirestore.getInstance().collection("users").document(currentUserId());
+    }
+
+    public static CollectionReference allUserCollectionReference(){
+        return FirebaseFirestore.getInstance().collection("users");
+    }
+
+    public static DocumentReference getChatRoomReference(String chatRoomId){
+        return FirebaseFirestore.getInstance().collection("chatrooms").document(chatRoomId);
+    }
+
+    public static CollectionReference getChatroomMessageReference(String chatroomId){
+        return getChatRoomReference(chatroomId).collection("chats");
+    }
+
+    public static String getChatroomId(String userId1, String userId2){
+        if (userId1.hashCode()<userId2.hashCode()){
+            return userId1+"_"+userId2;
+        } else {
+            return userId2+"_"+userId1;
+        }
+    }
+
+}
