8a4f78aaadacb07d8c7afcbc7c7befe490ae0c12
ducloc <locdivine@gmail.com>
2023-06-27 22:55:19 +0700

    Create recent chat

--- app/src/main/java/com/lucky/social_media_lemon/ChatActivity.java
+++ app/src/main/java/com/lucky/social_media_lemon/ChatActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -54,6 +55,14 @@ public class ChatActivity extends AppCompatActivity {
         otherUserName = findViewById(R.id.other_username);
         recyclerView = findViewById(R.id.chat_recycler_view);
         imageView =findViewById(R.id.profile_pic_image_view);
+
+//        FirebaseUtil.getOtherProfilePicStorageRef(otherUser.getUserId()).getDownloadUrl()
+//                .addOnCompleteListener(t -> {
+//                    if (t.isSuccessful()){
+//                        Uri uri = t.getResult();
+//                        AndroidUtil.setProfilePic(this, uri, imageView);
+//                    }
+//                });
 
         backBtn.setOnClickListener(v -> {
             onBackPressed();
